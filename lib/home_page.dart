8ba891410cb303599edafde907fc3c97050af86b
belarminon <35656300+belarminon@users.noreply.github.com>
2020-04-30 23:28:57 -0300

    Criei o HomePage

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Brincando aqui gostoso'),
+        centerTitle: true,
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(color: Colors.yellow);
+  }
+}
